5815d3edf794f460b80229395039afcb75fd1285
karthik <karthikeyasurampudi29@gmail.com>
2023-12-19 23:41:02 +0530

    mini change in exception file

--- src/exception.py
+++ src/exception.py
@@ -21,11 +21,3 @@ class CustomException(Exception):
 
     def __str__(self) -> str:
         return self.error_message
-
-
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divison by Zero")
-        raise CustomException(e, sys)
