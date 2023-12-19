d3f2b98ae6a9e983b97ffe71abcb0ccd22bb9d24
karthik <karthikeyasurampudi29@gmail.com>
2023-12-20 00:35:05 +0530

    data ingestion phase is added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class DataIngestConfig:
+    train_data_path = os.path.join("artifact", "train.csv")
+    test_data_path = os.path.join("artifact", "test.csv")
+    raw_data_path = os.path.join("artifact", "data.csv")
+
+
+class DataIngest:
+    def __init__(self):
+        self.ingestion_config = DataIngestConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the Data Ingestion Phase")
+        try:
+            df = pd.read_csv(
+                r"C:\Users\Lenovo\Desktop\webapps\MLproject\notebooks\data\stud.csv"
+            )
+            logging.info("Dataset is exported")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(self.ingestion_config.raw_data_path, header=True, index=False)
+            logging.info("Train and test split is initiated")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path)
+            test_set.to_csv(self.ingestion_config.test_data_path)
+
+            logging.info("Ingestion is completed")
+
+            return (
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path,
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngest()
+    obj.initiate_data_ingestion()
